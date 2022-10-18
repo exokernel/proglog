570e5826e05aab5d80fa266ceb6f3b2b0139ec29
Dustin Cannon <dustin.cannon@bigcommerce.com>
2022-10-17 22:44:05 -0400

    read

--- internal/log/store.go
+++ internal/log/store.go
@@ -50,3 +50,20 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.size += uint64(w)
 	return uint64(w), pos, nil
 }
+
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
