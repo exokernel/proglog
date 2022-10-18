df8171347b34df67f17d379059fa1284d84b2554
Dustin <dustin@shunyata.space>
2022-10-17 21:58:34 -0400

    store

---
+++ internal/log/store.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
