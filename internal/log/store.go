7b63b5402de6717ba264ce8fafb2afe1d39c3051
Dustin Cannon <dustin.cannon@bigcommerce.com>
2022-10-17 22:27:20 -0400

    store new, append

--- internal/log/store.go
+++ internal/log/store.go
@@ -21,3 +21,32 @@ type store struct {
 	buf  *bufio.Writer
 	size uint64
 }
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
