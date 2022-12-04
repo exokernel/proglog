5f7c2040e9fc3094d1d71d83b74cc1db8a52e841
Dustin <dustin@shunyata.space>
2022-12-03 18:05:32 -0800

    final segment method

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -85,3 +85,37 @@ func (s *segment) Read(off uint64) (*api.Record, error) {
 	err = proto.Unmarshal(p, record)
 	return record, nil
 }
+
+func (s *segment) isMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes || s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
+}
