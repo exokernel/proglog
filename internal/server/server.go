71a69a6283ee5f26454e5b24971d616776d55b3f
Dustin Cannon <dustin@shunyata.space>
2023-01-13 01:29:59 -0800

    started on grpc server

---
+++ internal/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/exokernel/proglog/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(conifg *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ConsumeResponse{Record: record}, nil
+}
